8e52da803ceb17df68026f285626cb2df532cb5a
Matej De Faveri <md2085@student.uni-lj.si>
2020-08-23 20:55:50 +0200

    Not sure?

--- src/js/main.js
+++ src/js/main.js
@@ -34,15 +34,42 @@ $(document).click(function(e) {
 let deferredPrompt; // Allows to show the install prompt
 var installButton = document.getElementById("installPWA");
 
-window.addEventListener("beforeinstallprompt", event => {
+   window.addEventListener('beforeinstallprompt', function(event) {
+		installButton.hidden = false;
+		
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
+   });
+
+   // Installation must be done by a user gesture! Here, the button click
+   installButton.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		installButton.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice
+			.then((choiceResult) => {
+				if (choiceResult.outcome === 'accepted') {
+					console.log('User accepted the A2HS prompt');
+				} else {
+					console.log('User dismissed the A2HS prompt');
+				}
+				deferredPrompt = null;
+			});
+   });
+
+/* window.addEventListener("beforeinstallprompt", event => {
 	event.preventDefault();
 	deferredPrompt = event;
 });
 
 // when installation completed
-/* window.addEventListener("appinstalled", evt => {
+window.addEventListener("appinstalled", evt => {
   console.log("appinstalled", evt);
-}); */
+});
 
 installButton.addEventListener("onclick", (e) => {
 	installButton.hidden = false;
@@ -57,7 +84,7 @@ installButton.addEventListener("onclick", (e) => {
 		installButton.disabled = false;
 		deferredPrompt = null;
 	});
-})
+}) */
 
 // ************************************************************* -- END
 

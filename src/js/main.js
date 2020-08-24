67ecafef0085bfad306d9264a7b72e1c4cd84078
Matej De Faveri <md2085@student.uni-lj.si>
2020-08-24 02:33:13 +0200

    no groove 'till you

--- src/js/main.js
+++ src/js/main.js
@@ -50,32 +50,33 @@ $(document).click(function(e) {
 let deferredPrompt; // Allows to show the install prompt
 var installButton = document.getElementById("installPWA");
 
-if (window.matchMedia('(display-mode: standalone)').matches || displayMode == "browser tab") {  
+if (window.matchMedia("(display-mode: standalone)").matches) { //displayMode == "browser tab"
    installButton.hidden = true;
 } 
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+	// Prevent the mini-infobar from appearing on mobile
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+	// Update UI notify the user they can install the PWA
+	installButton.hidden = false;
 });
 
-installButton.addEventListener('click', (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-  });
+installButton.addEventListener("click", (e) => {
+	// Hide the app provided install promotion
+	hideMyInstallPromotion();
+	// Show the install prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice.then((choiceResult) => {
+		if (choiceResult.outcome === "accepted") {
+			console.log("User accepted the install prompt");
+			installButton.hidden = true;
+		} else {
+			console.log("User dismissed the install prompt");
+		}
+	});
 });
 
 // ************************************************************* -- END

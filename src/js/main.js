0a4de5b4253f9c2837cb772c08b866b367db114a
Matej De Faveri <md2085@student.uni-lj.si>
2020-08-24 02:22:00 +0200

    Little jazz

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
+// Check display mode ******************************************************************************
+let displayMode = 'browser tab';
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (navigator.standalone) {
+    displayMode = 'standalone-ios';
+  }
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    displayMode = 'standalone';
+  }
+});
+
+// ************************************************************* -- END
+
+
 // SW script ****************************************************************************************
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
@@ -34,59 +50,33 @@ $(document).click(function(e) {
 let deferredPrompt; // Allows to show the install prompt
 var installButton = document.getElementById("installPWA");
 
-if (window.matchMedia('(display-mode: standalone)').matches) {  
+if (displayMode == "browser tab") {  
    installButton.hidden = true;
 } 
 
-window.addEventListener('beforeinstallprompt', function(event) {
-	// Prevent Chrome 67 and earlier from automatically showing the prompt
-	event.preventDefault();
-	// Stash the event so it can be triggered later.
-	deferredPrompt = event;
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
 });
 
-// Installation must be done by a user gesture! Here, the button click
 installButton.addEventListener('click', (e) => {
-	// hide our user interface that shows our A2HS button
-	installButton.style.display = 'none';
-	// Show the prompt
-	deferredPrompt.prompt();
-	// Wait for the user to respond to the prompt
-	deferredPrompt.userChoice
-		.then((choiceResult) => {
-			if (choiceResult.outcome === 'accepted') {
-				console.log('User accepted the A2HS prompt');
-			} else {
-				console.log('User dismissed the A2HS prompt');
-			}
-			deferredPrompt = null;
-		});
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
 });
-
-/* window.addEventListener("beforeinstallprompt", event => {
-	event.preventDefault();
-	deferredPrompt = event;
-});
-
-// when installation completed
-window.addEventListener("appinstalled", evt => {
-  console.log("appinstalled", evt);
-});
-
-installButton.addEventListener("onclick", (e) => {
-	installButton.hidden = false;
-	deferredPrompt.prompt();
-	installButton.disabled = true;
-
-	// Wait for the user to respond to the prompt
-	deferredPrompt.userChoice.then(choiceResult => {
-		if (choiceResult.outcome === "accepted") {
-			installButton.hidden = true;
-		}
-		installButton.disabled = false;
-		deferredPrompt = null;
-	});
-}) */
 
 // ************************************************************* -- END
 

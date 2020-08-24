bdde6e348f3fc2c09332a7ca1d597b46069100ca
Matej De Faveri <md2085@student.uni-lj.si>
2020-08-24 02:45:58 +0200

    well

--- src/js/main.js
+++ src/js/main.js
@@ -54,6 +54,31 @@ if (window.matchMedia("(display-mode: standalone)").matches) {
    installButton.hidden = true;
 } 
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  installButton.hidden = false;
+
+  installButton.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    installButton.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
 /* window.addEventListener("beforeinstallprompt", (e) => {
 	// Prevent the mini-infobar from appearing on mobile
 	e.preventDefault();
@@ -61,7 +86,7 @@ if (window.matchMedia("(display-mode: standalone)").matches) {
 	deferredPrompt = e;
 }); */
 
-installButton.addEventListener("click", (e) => {
+/* installButton.addEventListener("click", (e) => {
 	// Show the install prompt
 	deferredPrompt.prompt();
 	// Wait for the user to respond to the prompt
@@ -73,7 +98,7 @@ installButton.addEventListener("click", (e) => {
 			console.log("User dismissed the install prompt");
 		}
 	});
-});
+}); */
 
 // ************************************************************* -- END
 

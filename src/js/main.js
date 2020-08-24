87b3d14321680d28a97c53fd3d6e2b240f5a0fc2
Matej De Faveri <md2085@student.uni-lj.si>
2020-08-24 02:06:41 +0200

    Fuck!

--- src/js/main.js
+++ src/js/main.js
@@ -38,30 +38,30 @@ if (window.matchMedia('(display-mode: standalone)').matches) {
    installButton.hidden = true;
 } 
 
-   window.addEventListener('beforeinstallprompt', function(event) {
-		// Prevent Chrome 67 and earlier from automatically showing the prompt
-		e.preventDefault();
-		// Stash the event so it can be triggered later.
-		deferredPrompt = e;
-   });
+window.addEventListener('beforeinstallprompt', function(event) {
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	event.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = event;
+});
 
-   // Installation must be done by a user gesture! Here, the button click
-   installButton.addEventListener('click', (e) => {
-		// hide our user interface that shows our A2HS button
-		installButton.style.display = 'none';
-		// Show the prompt
-		deferredPrompt.prompt();
-		// Wait for the user to respond to the prompt
-		deferredPrompt.userChoice
-			.then((choiceResult) => {
-				if (choiceResult.outcome === 'accepted') {
-					console.log('User accepted the A2HS prompt');
-				} else {
-					console.log('User dismissed the A2HS prompt');
-				}
-				deferredPrompt = null;
-			});
-   });
+// Installation must be done by a user gesture! Here, the button click
+installButton.addEventListener('click', (e) => {
+	// hide our user interface that shows our A2HS button
+	installButton.style.display = 'none';
+	// Show the prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+});
 
 /* window.addEventListener("beforeinstallprompt", event => {
 	event.preventDefault();

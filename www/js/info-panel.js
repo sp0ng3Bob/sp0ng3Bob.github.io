98a0152d3407cad7b094d8e70e34a3f9b4124790
sp0ng3Bob <14179704+sp0ng3Bob@users.noreply.github.com>
2023-08-24 19:01:19 +0200

    90sas89dpg29dglakgx8x

---
+++ www/js/info-panel.js
@@ -0,0 +1,43 @@
+AFRAME.registerComponent('info-panel', {
+  init: function () {
+    var fadeBackgroundEl = this.fadeBackgroundEl = document.querySelector('#fadeBackground');
+		let exitButton = document.querySelector('#closeInfo')
+
+    this.onBackgroundClick = this.onBackgroundClick.bind(this);
+    this.backgroundEl = document.querySelector('#background');
+    exitButton.addEventListener('click', (e) => {
+			console.log("EXIT!", e)
+		}) //this.onExitButtonClick.bind(this));
+    this.backgroundEl.addEventListener('click', this.onBackgroundClick);
+    this.el.object3D.renderOrder = 9999999;
+    this.el.object3D.depthTest = false;
+    fadeBackgroundEl.object3D.renderOrder = 9;
+    fadeBackgroundEl.getObject3D('mesh').material.depthTest = false;
+  },
+
+  onExitButtonClick: function (evt) {
+		console.log("EXIT!", evt)
+    /*var movieInfo = this.movieInfo[evt.currentTarget.id];
+
+    this.backgroundEl.object3D.scale.set(1, 1, 1);
+
+    this.el.object3D.scale.set(1, 1, 1);
+    if (AFRAME.utils.device.isMobile()) { this.el.object3D.scale.set(1.4, 1.4, 1.4); }
+    this.el.object3D.visible = true;
+    this.fadeBackgroundEl.object3D.visible = true;
+
+    if (this.movieImageEl) { this.movieImageEl.object3D.visible = false; }
+    this.movieImageEl = movieInfo.imgEl;
+    this.movieImageEl.object3D.visible = true;
+
+    this.movieTitleEl.setAttribute('text', 'value', movieInfo.title);
+    this.movieDescriptionEl.setAttribute('text', 'value', movieInfo.description);*/
+  },
+
+  onBackgroundClick: function (evt) {
+    this.backgroundEl.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.scale.set(0.001, 0.001, 0.001);
+    this.el.object3D.visible = false;
+    this.fadeBackgroundEl.object3D.visible = false;
+  }
+});

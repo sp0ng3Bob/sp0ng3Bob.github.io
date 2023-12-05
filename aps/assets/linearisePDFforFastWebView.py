e75a6372efc593e42fa57fa7fb80a2a858b1226b
sp0ng3Bob <14179704+sp0ng3Bob@users.noreply.github.com>
2023-12-05 14:00:09 +0100

    Arriyadh plants searcher #6

--- aps/assets/linearisePDFforFastWebView.py
+++ aps/assets/linearisePDFforFastWebView.py
@@ -7,5 +7,7 @@ def linearize_pdf(input_path, output_path):
 
 #linearize_pdf('./book/Arriyadh plants.pdf', './book/Arriyadh plants-fwv.pdf')
 
-for pdf in Path("./book parts/").glob("**/*.pdf"):
-    linearize_pdf(pdf,f"{str(pdf)[:-4]}-fwv.pdf")
+#for pdf in Path("./book parts/").glob("**/*.pdf"):
+#    linearize_pdf(pdf,f"{str(pdf)[:-4]}-fwv.pdf")
+
+linearize_pdf('./book parts/Rest of the book.pdf', './book parts/Rest of the book-fwv.pdf')

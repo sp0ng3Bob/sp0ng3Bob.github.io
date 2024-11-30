b43fee9b3d87dd373809600f78a5db67dc735dff
sp0ng3Bob <14179704+sp0ng3Bob@users.noreply.github.com>
2024-11-30 18:47:25 +0100

    IG: idk

--- apps/ig/src/scripts/app.js
+++ apps/ig/src/scripts/app.js
@@ -2,6 +2,9 @@ import Data from './data.js';
 import GUI from './gui.js';
 //import Routing from './routing.js'
 
+const navigation = document.querySelector("#navigation")
+const hideNavButton = document.querySelector("#hideNavigation")
+
 // Create instances of Data and GUI classes
 const data = new Data();
 const gui = new GUI(data);
@@ -12,3 +15,8 @@ const gui = new GUI(data);
 
   console.log("GUI fully initialized!");
 })();
+
+/* hideNavButton.addEventListener("click", () => {
+  navigation.classList.remove("active");
+  navigation.blur();
+}) */
